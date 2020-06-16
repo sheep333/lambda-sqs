6b8d6478ea395bc359568745aed5a4121a531702
sheep333 <yukiko.elike1.51@gmail.com>
2020-06-16 23:02:54 +0900

    SQSから受け取るメッセージデータのサンプルを修正

--- ProcessSQSRecords.py
+++ ProcessSQSRecords.py
@@ -13,24 +13,26 @@ def lambda_handler(event, context):
 
     """
     受け取る辞書データ(属性追加も可能)
-    {
-        "messageId": "2e1424d4-f796-459a-8184-9c92662be6da",
-        "receiptHandle": "AQEBzWwaftRI0KuVm4tP+/7q1rGgNqicHq...",
-        "body": "Test message.",
-        "attributes": {
-            "ApproximateReceiveCount": "1",
-            "SentTimestamp": "1545082650636",
-            "SenderId": "AIDAIENQZJOLO23YVJ4VO",
-            "ApproximateFirstReceiveTimestamp": "1545082650649"
-        },
-        "messageAttributes": {
-            "UUID": "xxxxxxxxx"
-        },
-        "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
-        "eventSource": "aws:sqs",
-        "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
-        "awsRegion": "us-east-2"
-    }
+    "Records": [
+        {
+            "messageId": "2e1424d4-f796-459a-8184-9c92662be6da",
+            "receiptHandle": "AQEBzWwaftRI0KuVm4tP+/7q1rGgNqicHq...",
+            "body": "Test message.",
+            "attributes": {
+                "ApproximateReceiveCount": "1",
+                "SentTimestamp": "1545082650636",
+                "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+                "ApproximateFirstReceiveTimestamp": "1545082650649"
+            },
+            "messageAttributes": {
+                "UUID": "xxxxxxxxx"
+            },
+            "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+            "eventSource": "aws:sqs",
+            "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+            "awsRegion": "us-east-2"
+        }
+    ]
     """
 
     try:
