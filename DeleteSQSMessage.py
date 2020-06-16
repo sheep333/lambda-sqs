8debc4080b60ff602ba4be8c3dd4a2f4f975235b
sheep333 <yukiko.elike1.51@gmail.com>
2020-06-16 23:06:09 +0900

    SQSのキュー削除方法を変更

--- DeleteSQSMessage.py
+++ DeleteSQSMessage.py
@@ -5,16 +5,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 sqs = boto3.client('sqs')
+queue = sqs.Queue("queue_url")
 
 
 def lambda_handler(event, context):
     try:
-        name = "queue_name"  # TODO:どこかで設定ファイルや環境変数から読み込み？もしくはJSONとして受取り？
-        url = sqs.get_queue_url(
-            QueueName=name,
-        )
         sqs.delete_message(
-            QueueUrl=url,
+            QueueUrl="queue_url",
             ReceiptHandle=context['receiptHandle']
         )
     except Exception as e:
